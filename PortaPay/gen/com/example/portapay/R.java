7d002170dc9277abfc6705ec315dc9fe3abf5694
Nargarawr <cxk01u@gmail.com>
2013-10-12 21:51:36 +0100

    maps in

--- PortaPay/gen/com/example/portapay/R.java
+++ PortaPay/gen/com/example/portapay/R.java
@@ -25,11 +25,10 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
-        public static final int btn_launchMap=0x7f080000;
+        public static final int map_container=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_map=0x7f030000;
-        public static final int activity_porta_pay_main=0x7f030001;
+        public static final int activity_porta_pay_main=0x7f030000;
     }
     public static final class menu {
         public static final int map=0x7f070000;
--- PortaPay/src/com/example/portapay/PortaPayMain.java
+++ PortaPay/src/com/example/portapay/PortaPayMain.java
@@ -1,7 +1,11 @@
 package com.example.portapay;
 
+import com.google.android.gms.maps.MapFragment;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
@@ -17,13 +21,12 @@ public class PortaPayMain extends Activity {
         setContentView(R.layout.activity_porta_pay_main);
         this.ppMain = this;  
 		
-        final Button button = (Button) findViewById(R.id.btn_launchMap);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent mapDrawIntent = new Intent (ppMain, MapActivity.class) ;
-                startActivity(mapDrawIntent);
-            }
-        });
+        MapFragment mMapFragment = MapFragment.newInstance();
+        FragmentTransaction fragmentTransaction =
+                getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.map_container, mMapFragment);
+        fragmentTransaction.commit();
+        
     }
 
 
