4114ebb1dd3f91b623cd29e2faee298cfff8d494
Nargarawr <cxk01u@gmail.com>
2013-10-12 19:43:31 +0100

    Launching of MapActivity working

--- PortaPay/src/com/example/portapay/PortaPayMain.java
+++ PortaPay/src/com/example/portapay/PortaPayMain.java
@@ -2,22 +2,26 @@ package com.example.portapay;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
 public class PortaPayMain extends Activity {
 
+	static public Activity ppMain;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_porta_pay_main);
-        
+        this.ppMain = this;  
 		
         final Button button = (Button) findViewById(R.id.btn_launchMap);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Perform action on click
+                Intent mapDrawIntent = new Intent (ppMain, MapActivity.class) ;
+                startActivity(mapDrawIntent);
             }
         });
     }
